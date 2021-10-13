81ddcdf2361513cae5366ee18721aba321b1a463
L YX <liuyunzhhereinafter@foxmail.com>
2021-10-13 12:10:20 +0800

    ADD  input visualization & bert encode

---
+++ bert_encode.py
@@ -0,0 +1,48 @@
+import torch
+from transformers import *
+import transformers as tfs
+import os
+import tensorflow
+import numpy as np
+
+
+class bert_encoder:
+    model: BeitModel
+    tokenizer_class: BertTokenizer
+
+    def __init__(self, pretrained_model_path):
+        self.model = BertModel.from_pretrained(pretrained_model_path, from_tf=True)
+        self.tokenizer_class = BertTokenizer.from_pretrained(pretrained_model_path)
+
+    def chinese2encode_bert(self, sentences):
+        assert type(sentences) is tuple
+
+        train_tokenized = [self.tokenizer_class.encode(test) for test in sentences]
+        print(train_tokenized)
+        train_max_len = 0
+        for i in train_tokenized:
+            if len(i) > train_max_len:
+                train_max_len = len(i)
+
+        padded = np.array([i + [0] * (train_max_len - len(i)) for i in train_tokenized])
+        print("train set shape:", padded.shape)
+
+        attention_mask = np.where(padded != 0, 1, 0)
+
+        input_ids = torch.tensor(padded)
+        attention_mask = torch.tensor(attention_mask)
+
+        with torch.no_grad():
+            last_hidden_states = self.model(input_ids, attention_mask=attention_mask)
+
+        features = last_hidden_states[0][:, 0, :].numpy()
+
+        print(features.shape)
+        print(features)
+
+
+if __name__ == '__main__':
+    pretrained_model = "D:/ML/transformer/bert-base-chinese/"
+    st = ("这是一个测试语句", '这也是一个测试语句')
+    bert_encoder1 = bert_encoder(pretrained_model)
+    bert_encoder1.chinese2encode_bert(st)
--- input.py
+++ input.py
@@ -1,4 +1,9 @@
-import pandas
+import numpy as np
+import copy
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from collections import Counter
 
 
 def read_data(file_path, is_train_data=True):
@@ -30,12 +35,63 @@ def read_data(file_path, is_train_data=True):
             index += 1
     m_ids = {'script_ids': script_ids, 'scene_nums': scene_nums, 'sentence_nums': sentence_nums, 'ids': ids}
     if is_train_data:
-        return m_ids, content, characters, emotions
+        return {'ids': m_ids, 'contents': contents, 'characters': characters, 'emotions': emotions}
     else:
-        return m_ids, content, characters
+        return {'ids': m_ids, 'contents': contents, 'characters': characters}
+
+
+def delete_empty_data(data):
+    copy_data = copy.deepcopy(data)
+    emotions = copy_data['emotions']
+    rm = []
+    for i in range(len(emotions)):
+        emotion = emotions[i]
+        if len(emotion) == 0:
+            rm.append(i)
+    for key in copy_data:
+        ele = copy_data[key]
+        if type(ele) is dict:
+            for ids_key in ele:
+                ids = ele[ids_key]
+                assert type(ids) is list
+                for i in range(len(rm) - 1, -1, -1):
+                    del ids[rm[i]]
+        else:
+            assert type(ele) is list
+            for i in range(len(rm) - 1, -1, -1):
+                del ele[rm[i]]
+
+    return copy_data
+
+
+def split_emotion(emotions):
+    new_emotions = [[int(level) for level in emotion_as_str.split(',')]
+                    for emotion_as_str in emotions]
+    return new_emotions
 
 
 if __name__ == '__main__':
-    data_path = "D:/ML/transformer/test_dataset.tsv"
-    data = read_data(data_path, False)
-    print(len(data[0]['ids']))
+    test_data_path = "D:/ML/transformer/test_dataset.tsv"
+    train_data_path = "D:/ML/transformer/train_dataset_v2.tsv"
+    data = read_data(train_data_path)
+    data_with_label = delete_empty_data(data)
+    print("before: {0}".format(len(data['contents'])))
+    print('after: {0}'.format(len(data_with_label['contents'])))
+    data_with_label['emotions'] = split_emotion(data_with_label['emotions'])
+    emotions = np.array(data_with_label['emotions'])
+
+    for i in range(6):
+        plt.figure('emotion{0} his'.format(i))
+        plt.hist(emotions[:, i])
+        total = len(emotions)
+        times = Counter(emotions[:, i])
+        print('emotion {0}: {1}'.format(i, times))
+        per = []
+        num = []
+        for j in range(len(times)):
+            per.append(times[j] / total)
+            num.append(times[j])
+        print('emotion {0}: proportion {1}'.format(i, per))
+        plt.figure('emotion{0} pie'.format(i))
+        plt.pie(num)
+    plt.show()
